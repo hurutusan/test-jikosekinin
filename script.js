f40b997d102a42843b1bfc9519f173da406baae8
moti <118610476+hurutusan@users.noreply.github.com>
2024-08-07 13:49:55 +0900

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const resultDiv = document.getElementById('result');
 
 updateButton.addEventListener('click', () => {
   // ScratchのAPIを呼び出す
-  fetch('https://api.scratch.mit.edu/users/YOUR_USERNAME/projects')
+  fetch('https://api.scratch.mit.edu/users/amedamasan/projects')
     .then(response => response.json())
     .then(data => {
       // 取得したデータから必要な情報を取り出し、resultDivに表示する
